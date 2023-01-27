c1bf1b5c1679c10af2772a404881fed525faa4cc
FranKBrownn <Frankcdbrown97@gmail.com>
2023-01-27 15:43:07 +0000

    set my my admin and created the database ready to add user bookings

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Plublished"))
+
+
+class Post(models.Model):
+    fname = models.CharField(max_length=15)
+    lname = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    number = models.IntegerField()
+    date = models.DateTimeField()
+    notes = models.CharField(max_length=50)
+
+
+

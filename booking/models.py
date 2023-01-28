2642e6774a8122e3749f722fef9bcd22a034d248
FranKBrownn <Frankcdbrown97@gmail.com>
2023-01-28 14:55:13 +0000

    added the templates ready for editing

--- booking/models.py
+++ booking/models.py
@@ -6,12 +6,15 @@ STATUS = ((0, "Draft"), (1, "Plublished"))
 
 
 class Post(models.Model):
-    fname = models.CharField(max_length=15)
-    lname = models.CharField(max_length=15)
-    email = models.EmailField(unique=True)
-    number = models.IntegerField()
-    date = models.DateTimeField()
-    notes = models.CharField(max_length=50)
+    fname = models.CharField(max_length=15, blank=False)
+    lname = models.CharField(max_length=15, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    number = models.IntegerField(blank=False)
+    date = models.DateTimeField(blank=False)
+    notes = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.fname
 
 
 
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostBooking():
+    template_name =''

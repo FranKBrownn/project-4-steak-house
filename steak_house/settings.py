91e6ecae35988b48c651d83adcd862d77b35fa7a
FranKBrownn <Frankcdbrown97@gmail.com>
2023-02-02 21:05:38 +0000

    get everything ready for a heruku deployment

--- steak_house/settings.py
+++ steak_house/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["steak-house97.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -87,16 +87,16 @@ WSGI_APPLICATION = 'steak_house.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 

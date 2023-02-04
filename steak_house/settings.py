32d3b58940031f7a2eb206f587be4d2c0ecb4a7d
FranKBrownn <Frankcdbrown97@gmail.com>
2023-02-04 10:25:35 +0000

    fixed some deployment issues

--- steak_house/settings.py
+++ steak_house/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["steak-house97.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["steak-house97.herokuapp.com", "localhost"]
 
 
 # Application definition
